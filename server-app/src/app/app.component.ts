91dfcfda14e7d24aacb0c102e3181995df32df31
Obieda Akilan <obieda.jehad@gmail.com>
2023-09-02 16:08:16 +0300

    In this section I learned how share date between parent and child and vise versa and I have learned about @ViewChild to selecte the referance template and I have looked on lifecycle of components

--- server-app/src/app/app.component.ts
+++ server-app/src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Server } from './server-element/server.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  servers: string[] = [];
-  serverName: string = 'the default server';
-  addNewServer() {
-    this.servers.push(this.serverName);
+  serverElements: Server[] = [];
+
+  onServerAdded(serverData: { name: string; content: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.name,
+      content: serverData.content,
+    });
   }
 
-  removeServer(index: number) {
-    const position = index + 1;
-    this.servers.splice(position, 1);
+  onBlueprintAdded(blueprintData: { name: string; content: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.name,
+      content: blueprintData.content,
+    });
   }
 }
